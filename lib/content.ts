94236636baacbcb255984316c5ea8439599cf29b
Arithach Boss Chartariyapong <arithachbossabc@Arithachs-MacBook-Pro.local>
2026-06-24 21:01:07 +0700

    Correct founder Thai name

--- lib/content.ts
+++ lib/content.ts
@@ -84,7 +84,7 @@ export const footerLinks = [
 
 export const partner = {
   founder: "Arithach Chartariyapong",
-  founderThai: "อริทัช ริยะพงษ์",
+  founderThai: "อริย์ธัช ชาติอาริยะพงศ์",
   technologyPartner: "Asian Coding Systems (ACS)",
   icon: Landmark
 };
